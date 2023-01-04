0b4efda93286e763efea35ce9d558b86fb0a43d8
20N31A0318 <20n31a0318@mrcet.ac.in>
2023-01-05 01:10:15 +0530

    added dueToday check

--- models/todo.js
+++ models/todo.js
@@ -95,7 +95,11 @@ module.exports = (sequelize, DataTypes) => {
 
     displayableString() {
       let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      return `${this.id}. ${checkbox} ${this.title} ${
+        this.dueDate == new Date().toLocaleDateString("en-CA")
+          ? ""
+          : this.dueDate
+      }`.trim();
     }
   }
   Todo.init(
